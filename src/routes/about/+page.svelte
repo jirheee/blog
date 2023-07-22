51727b12f67a2dad15edfbae588115647cf2a841
JungInRhee <ysrheee@kaist.ac.kr>
2023-07-23 01:01:40 +0900

    add portrait

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,7 @@
 <h1>About</h1>
 
+<img class="portrait" src="/jungin.rhee.jpg" alt="jung in's picutre" />
+
 <p>
 	I am a MS student at <a href="https://cp.kaist.ac.kr/">Concurrency and Parallelism Laboratory</a>,
 	<a href="https://cs.kaist.ac.kr/">KAIST School of Computing</a>.
@@ -14,3 +16,10 @@
 	Currently I am building a high-level Hardware Description Language, which is designed to be easily
 	programmable, offering an accessible platform without compromising on performance.
 </p>
+
+<style>
+	.portrait {
+		margin: var(--size-3);
+		border-radius: 8px;
+	}
+</style>
